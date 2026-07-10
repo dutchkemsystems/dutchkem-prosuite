a0744485aaae2944933acde2c56c0f5978ecd53a
dutchkemsystems <dutchkemsystems@gmail.com>
2026-07-10 17:03:11 +0100

    fix: add missing internalMutation import to social/core.ts

--- convex/social/core.ts
+++ convex/social/core.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { action, internalQuery, mutation, query } from "../_generated/server";
+import { action, internalMutation, internalQuery, mutation, query } from "../_generated/server";
 import { internal } from "../_generated/api";
 import { tryGetAdminSession, tryGetAdminSessionInAction } from "../auth_helpers";
 import { PLATFORM_CONFIGS } from "./platform_configs";
